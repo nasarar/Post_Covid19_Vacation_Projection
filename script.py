2aa0816ba93b34a5785be7265b827d57dc8e3016
Zainak94 <zainak94@gmail.com>
2021-06-07 21:18:14 -0400

    adding the changes in script

--- script.py
+++ script.py
@@ -3,21 +3,22 @@ import sqlite3
 import pandas as pd
   
 # Connect to SQLite database
-conn = sqlite3.connect('forecast_all.db')
+conn = sqlite3.connect('world_data_forecast.db')
 c= conn.cursor()
 
 ## Execute a query 
-c.execute('''CREATE TABLE forecast_herdimmunity (country TEXT, date TEXT, prediction INT)''')
+c.execute('''CREATE TABLE world_forecast_herdimmunity (location TEXT, date TEXT, people_fully_vaccinated INT)''')
 
 # Load CSV data into Pandas DataFrame
-forecast_herdimmunity = pd.read_csv('Resources/---.csv')
+world_forecast_herdimmunity = pd.read_csv('Resources/world_data_forecast.csv')
 
 # Write the data to a sqlite table
-forecast_herdimmunity.to_sql('forecast_herdimmunity', conn, if_exists='append', index=False)
+world_forecast_herdimmunity.to_sql('world_forecast_herdimmunity', conn, if_exists='append', index=False)
   
-c.execute('''SELECT * FROM forecast_herdimmunity''').fetchall()
+c.execute('''SELECT * FROM world_forecast_herdimmunity''').fetchall()
 
 c.close()
 
-
-
+#Convert the CSV to JSON string using Python 
+df = pd.read_csv (r'Resources/world_data_forecast.csv')
+df.to_json (r'static/world_data_forecast.json')
