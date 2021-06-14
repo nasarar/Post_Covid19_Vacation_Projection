5f82ff6d089160c3909530363cf6229b1087b49a
Joseph Thomas <75267605+josephthomas08@users.noreply.github.com>
2021-06-14 11:31:31 -0400

    Update config.js

--- Website/static/js/config.js
+++ Website/static/js/config.js
@@ -1,4 +1,4 @@
-// API key
+# API key
 API_KEY = "pk.eyJ1IjoiYW1hbmRhaGFudG9ueSIsImEiOiJja21reDQ3Nm0wOHlwMnZvZDBmYW92NXB2In0.zHmiKSDFYiuwDRjrlVbCsg";
 API_KEY_TWO = "8d32007474ce2f1184533cb9ce09c196"
 PLACES_API = "AIzaSyCBV-5srDO8pcs8Ck_kmPfB8i_Vygunr_k"
