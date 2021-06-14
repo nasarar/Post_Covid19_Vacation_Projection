b942a3c01db21abd4b32e813f7e74eb6acaa7e62
Joseph Thomas <75267605+josephthomas08@users.noreply.github.com>
2021-06-14 11:30:29 -0400

    Update config.js
    "removed const"

--- Website/static/js/config.js
+++ Website/static/js/config.js
@@ -1,4 +1,4 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiYW1hbmRhaGFudG9ueSIsImEiOiJja21reDQ3Nm0wOHlwMnZvZDBmYW92NXB2In0.zHmiKSDFYiuwDRjrlVbCsg";
-const API_KEY_TWO = "8d32007474ce2f1184533cb9ce09c196"
-const PLACES_API = "AIzaSyCBV-5srDO8pcs8Ck_kmPfB8i_Vygunr_k"
+API_KEY = "pk.eyJ1IjoiYW1hbmRhaGFudG9ueSIsImEiOiJja21reDQ3Nm0wOHlwMnZvZDBmYW92NXB2In0.zHmiKSDFYiuwDRjrlVbCsg";
+API_KEY_TWO = "8d32007474ce2f1184533cb9ce09c196"
+PLACES_API = "AIzaSyCBV-5srDO8pcs8Ck_kmPfB8i_Vygunr_k"
